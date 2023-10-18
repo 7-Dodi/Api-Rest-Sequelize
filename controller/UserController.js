bbfd3037683e5bfe0fb93dc799a96fb099f7925f
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-10-18 17:52:40 -0300

    refactor: Using the Redis Client as an API cache memory

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,4 +1,5 @@
 const Usuario = require('../model/Usuario');
+const Client = require('../database/redis');
 
 module.exports.listarUsuarios = async function (req, res) {
     const usuarios = await Usuario.findAll();
@@ -15,13 +16,25 @@ module.exports.adicionarUsuario = async function (req, res){
     }
   };
   
-module.exports.buscarUsuarios = async function(req, res){ //Deu erro
-    const user = await Usuario.findByPk(req.params.email); //Email é o Primary Key
-  
-    if(user){
-      res.status(200).send(user);
+module.exports.buscarUsuarios = async function(req, res){
+    console.time(); //Verificando o tempo de busca dessa informação
+    const cachedData = await Client.get(`user-${req.params.email}`);
+    console.timeEnd();
+
+    if(cachedData){ //Caso seja encontrado os dados
+      console.log("Data retrieved from cache memory");
+      res.status(200).send(JSON.parse(cachedData)); //Retorna os dados encontrados
     }else{
-      res.status(404).send('Usuário não encontrado');
+      //Caso não encontre, procura no Banco de Dados
+      const user = await Usuario.findByPk(req.params.email); //Email é o Primary Key
+      
+      if(user){
+        res.status(200).send(user); //Retorna os dados
+        Client.setEx(`user-${req.params.email}`, 300, JSON.stringify(user)); //Usando o método setEx para settar a key em perído de 3min
+        console.log("Data retrieved from the database and stored in cache memory");
+      }else{
+        res.status(404).send('Usuário não encontrado');
+      }
     }
   };
   
